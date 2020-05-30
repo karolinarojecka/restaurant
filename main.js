51ef998d2f5dc7b68156f84826838f9290e6e372
karolinarojecka <k.kasperuk@gmail.com>
2020-05-30 22:34:06 +0200

    scss, node, npm init

--- main.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector('.hamburger__icon--js');
-menu.addEventListener('click', () => {
-    const navigation = document.querySelector('.navigation--js');
-    navigation.classList.toggle(`navigation--open`);
-})
